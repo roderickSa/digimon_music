f9877d82212eb1f6d9751e2366285245f95ff27d
Roderick sanchez <rodericksanchezmeza@gmail.com>
2021-11-23 11:54:28 -0500

    change edit header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Home msg="Welcome to Your Vue.js App"/>
+  <Home />
 </template>
 
 <script>
@@ -18,9 +18,10 @@ export default {
 
 :root{
     --red: #ff3838;
+    --orange: #FF542B;;
 }
 *{
-    font-family: 'Nunito', sans-serif;
+    font-family: 'Roboto', sans-serif;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
@@ -28,8 +29,34 @@ export default {
     border: none;
     text-decoration: none;
     text-transform: capitalize;
-    transition: all .2s linear;
+    transition: .2s linear;
 }
+html{
+    font-size: 62.5%;
+    /* overflow-x: hidden; */
+    scroll-padding-top: 9rem;
+    scroll-behavior: smooth;
+}
+html::-webkit-scrollbar{
+    width: .8rem;
+}
+html::-webkit-scrollbar-track{
+    background: transparent;
+}
+/* html::-webkit-scrollbar-thumb{
+    background: #2196f3;
+    border-radius: 5rem;
+} */
+/* html::-webkit-scrollbar-thumb{
+    background: #2196f3;
+    border-radius: 5rem;
+}
+.box-container::-webkit-scrollbar {
+  background: #2196f3;
+  color: #fff;
+  width: .8rem;
+  border-radius: 5rem;
+} */
 *::selection{
     background-color: var(--red);
     color: #fff;
@@ -43,4 +70,59 @@ html{
 body{
     background: #f7f7f7;
 }
+.box-container{
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  overflow: auto;
+  background: var(--orange);
+}
+.box-description{
+  position: absolute;
+  top: -100%;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(255, 255, 255, .9);
+  text-align: center;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  word-break: break-word;
+}
+.box:hover .box-description,
+.box:hover img{
+  top: 0;
+  transform: scale(1.2);
+}
+.box-description p{
+  font-size: 1.4rem;
+  padding: 1.3rem;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+}
+.carrusel-digimon{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  gap: 1.5rem;
+}
+.box{
+  flex: 1 1 15rem;
+  height: 15rem;
+  position: relative;
+  box-shadow: 0 0.5rem 1rem rgb(0 0 0 / 30%);
+  border-radius: 50%;
+  overflow: hidden;
+  cursor: pointer;
+}
+.box img{
+  height: 100%;
+  border-radius: 50%;
+  object-fit: cover;
+}
+/* .box img:hover{
+  transform: scale(1.2);
+} */
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,39 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="header" id="header">
+    <div class="box-container">
+      <div class="carrusel-digimon">
+        <div v-for="digimon in digimons" :key="digimon.id" class="box">
+          <img :src="digimon.img" :alt="digimon.img">
+          <div class="box-description">
+            <p>{{digimon.music}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  props: {
-    msg: String
+  data() {
+    return {
+      digimons: []
+    }
   },
-  created:() => {
-    
+  props: {
+    // msg: String
+  },
+  async created () {
+    try {
+      const response = await fetch(`http://127.0.0.1:8001/api/digimons`, {
+          'method': 'POST'      
+      });
+      const data = await response.json();
+      this.digimons = data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 </script>
